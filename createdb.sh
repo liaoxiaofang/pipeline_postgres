ff56ef41386a27fe5ba6e7742ce1063f276b8e81
liaoxiaofang <420671437@qq.com>
2018-11-15 16:53:34 +0800

    修改

--- createdb.sh
+++ createdb.sh
@@ -3,10 +3,10 @@
 cp /tmp/pg_hba.conf /var/lib/postgresql/data
 #创建innode实例
 createdb innode -U postgres -E UTF8 -e
-#导入基础数据操作,innode-20181019null.sql据实际替换
-psql -d innode -U postgres -f /tmp/innode-20181019null.sql
 #创建innodb用户
 psql -d postgres -U postgres -c "CREATE USER innodb WITH PASSWORD '888888'"
 #给innodb用户授予innode实例权限
 psql -d postgres -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE innode to innodb"
+#导入基础数据操作,数据库用户postgres据应用外部配置文件所指定的用户修改，基础数据包innode-20181019null.sql据实际修改
+psql -d innode -U postgres -f /tmp/innode-20181019null.sql
 
