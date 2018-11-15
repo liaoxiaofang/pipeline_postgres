7d1fef877621fc778aee4cbb70d07193af514d86
liaoxiaofang <420671437@qq.com>
2018-11-15 16:45:19 +0800

    去掉su postgres切换

--- createdb.sh
+++ createdb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #将如果需要postgres以外的数据库用户连接，请将该文件覆盖到/var/lib/postgresql/data目录下(注：目前只允许postgres连接)
 cp /tmp/pg_hba.conf /var/lib/postgresql/data
-#以下是创建innode实例，导入基础数据操作
-su postgres 
+#创建innode实例
 createdb innode -U postgres -E UTF8 -e
+#导入基础数据操作,innode-20181019null.sql据实际替换
 psql -d innode -U postgres -f /tmp/innode-20181019null.sql
+#创建innodb用户
 psql -d postgres -U postgres -c "CREATE USER innodb WITH PASSWORD '888888'"
+#给innodb用户授予innode实例权限
 psql -d postgres -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE innode to innodb"
 
